c85981df3b6c534b8113d0de9220c6ef9582422c
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-05 23:11:31 +0530

    Tested New Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,66 @@
-import Button from "@/Shared/Button";
+import { Button } from "@/Shared/Button";
 
 type Props = {};
 
 export default function App({}: Props) {
   return (
     <>
-      <Button />
-      <div className="mb-4 bg-primary p-4 text-onPrimary shadow-elevation-1">
-        <h2 className="text-display-lg">Note Sync</h2>
-      </div>
-      <div className="mb-4 bg-primaryContainer p-4 text-onPrimaryContainer shadow-elevation-2">
-        <h2 className="text-display-md">Note Sync</h2>
-      </div>
-      <div className="mb-4 bg-secondary p-4 text-onSecondary shadow-elevation-3">
-        <h2 className="text-display-sm">Note Sync</h2>
-      </div>
-      <div className="bg-secondaryContainer p-4 text-onSecondaryContainer">
-        <h2 className="text-headline-lg">Note Sync</h2>
-      </div>
-      <div className="bg-tertiary p-4 text-onTertiary">
-        <h2 className="text-headline-md">Note Sync</h2>
-      </div>
-      <div className="bg-tertiaryContainer p-4 text-onTertiaryContainer">
-        <h2 className="text-headline-sm">Note Sync</h2>
-      </div>
-      <div className="bg-error p-4 text-onError">
-        <h2 className="text-title-lg">Note Sync</h2>
-      </div>
-      <div className="bg-errorContainer p-4 text-onErrorContainer">
-        <h2 className="text-title-md">Note Sync</h2>
-      </div>
-      <div className="bg-background p-4 text-onBackground">
-        <h2 className="text-title-sm">Note Sync</h2>
-      </div>
-      <div className="bg-surface p-4 text-onSurface">
-        <h2 className="text-body-lg">Note Sync</h2>
-      </div>
+      <div className="flex min-h-screen items-center justify-center gap-4">
+        <Button variant="outline" size="default">
+          Cancel
+        </Button>
+        <Button variant="default" size="default">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="mr-1 h-5 w-5 fill-onPrimary"
+          >
+            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+          </svg>
+          add
+        </Button>
+        <Button variant="default" size="default">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="mr-1 h-4 w-4"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 2c-1.716 0-3.408.106-5.07.31C3.806 2.45 3 3.414 3 4.517V17.25a.75.75 0 001.075.676L10 15.082l5.925 2.844A.75.75 0 0017 17.25V4.517c0-1.103-.806-2.068-1.93-2.207A41.403 41.403 0 0010 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Save
+        </Button>
 
-      <div className="mt-9 bg-surfaceContainerLowest p-4 text-onSurface">
-        <h2 className="text-body-md">Note Sync</h2>
-      </div>
-      <div className="bg-surfaceContainerLow p-4 text-onSurface">
-        <h2 className="text-body-sm">Note Sync</h2>
-      </div>
-      <div className="bg-surfaceContainer p-4 text-onSurface">
-        <h2 className="text-label-lg">Note Sync</h2>
-      </div>
-      <div className="bg-surfaceContainerHigh p-4 text-onSurface">
-        <h2 className="text-label-lg">Note Sync</h2>
-      </div>
-      <div className="bg-surfaceContainerHighest p-4 text-onSurface">
-        <h2 className="text-label-lg">Note Sync</h2>
+        <Button variant="default" size="icon" elevation="elevatedLow">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Button>
+
+        <Button variant="ghost" size="icon2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+          </svg>
+        </Button>
       </div>
     </>
   );
