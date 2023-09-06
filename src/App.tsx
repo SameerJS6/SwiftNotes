8395d1845462674fc52e22d6d1fb034b71801d19
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-06 22:28:20 +0530

    Routes Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,25 @@
-import { Button } from "@/Shared/Button";
+import Login from "@/Auth/Login";
+import SignUp from "@/Auth/SignUp";
+import { Button, buttonVariants } from "@/Shared/Button";
+import { Link, Route, Routes } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import ProtectedRoutes from "@/ProtectedRoutes";
 
 type Props = {};
 
 export default function App({}: Props) {
+  const { logout, currentUser } = useAuth();
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center gap-4">
-        <Button variant="outline" size="default">
-          Cancel
-        </Button>
-        <Button variant="default" size="default">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="mr-1 h-5 w-5 fill-onPrimary"
-          >
-            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-          </svg>
-          add
-        </Button>
-        <Button variant="default" size="default">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="mr-1 h-4 w-4"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 2c-1.716 0-3.408.106-5.07.31C3.806 2.45 3 3.414 3 4.517V17.25a.75.75 0 001.075.676L10 15.082l5.925 2.844A.75.75 0 0017 17.25V4.517c0-1.103-.806-2.068-1.93-2.207A41.403 41.403 0 0010 2z"
-              clipRule="evenodd"
-            />
-          </svg>
-          Save
-        </Button>
-
-        <Button variant="default" size="icon" elevation="elevatedLow">
+      <div className="my-12 flex flex-wrap items-center justify-center gap-4">
+        <Link
+          to="/"
+          className={buttonVariants({
+            variant: "default",
+            size: "icon",
+            elevation: "elevatedLow",
+          })}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -45,23 +28,94 @@ export default function App({}: Props) {
           >
             <path
               fillRule="evenodd"
-              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+              d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
               clipRule="evenodd"
             />
           </svg>
-        </Button>
-
-        <Button variant="ghost" size="icon2">
+        </Link>
+        {/* Account Button  */}
+        <Link
+          to="/account"
+          className={buttonVariants({
+            variant: "default",
+            size: "icon",
+            elevation: "elevatedLow",
+          })}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
             className="h-5 w-5"
           >
-            <path d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+            <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
           </svg>
+        </Link>
+        {!currentUser && (
+          <div className="flex items-center gap-4">
+            {/* Login Button  */}
+            <Link
+              to="/login"
+              className={buttonVariants({
+                variant: "default",
+                size: "default",
+              })}
+            >
+              Log in
+            </Link>
+            {/* Sign Up Button  */}
+            <Link
+              to="/signup"
+              className={buttonVariants({
+                variant: "outline",
+                size: "default",
+              })}
+            >
+              Sign Up
+            </Link>
+          </div>
+        )}
+
+        {/* Log Out Button  */}
+        <Button
+          onClick={logout}
+          disabled={!currentUser ? true : false}
+          variant="ghost"
+          size="default"
+        >
+          log out
         </Button>
       </div>
+      <main className="px-4">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <h1 className="text-center text-display-lg">This is Home Page</h1>
+            }
+          ></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoutes>
+                <h1 className="text-center text-display-lg">
+                  This is Account Page
+                </h1>
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <h1 className="text-center text-display-lg text-error">
+                Not Found Page
+              </h1>
+            }
+          ></Route>
+        </Routes>
+      </main>
     </>
   );
 }
