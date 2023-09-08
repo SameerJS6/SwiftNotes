c4ab97e8ea03db557c244b50ea64244cae31d119
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-08 18:16:44 +0530

    Theme Toggle Added (Temp)

---
+++ src/components/UI/DarkMode.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/Shared/Button";
+import { InnerMoon } from "@theme-toggles/react";
+import "@theme-toggles/react/css/InnerMoon.css";
+import { useState, useEffect } from "react";
+
+type DarkModeProps = {};
+
+export default function DarkMode({}: DarkModeProps) {
+  const currentTheme = localStorage.getItem("noteSyncTheme");
+  const [isDark, setIsDark] = useState(currentTheme === "dark");
+
+  useEffect(() => {
+    localStorage.setItem("noteSyncTheme", isDark ? "dark" : "light");
+    isDark
+      ? document.body.classList.add("darkMode")
+      : document.body.classList.remove("darkMode");
+  }, [isDark]);
+
+  return (
+    <Button
+      rippleColour="rgb(var(--on-secondary-container), 0.1)"
+      className="group"
+      variant="ghostSurface"
+      size="icon"
+    >
+      <InnerMoon toggled={isDark} toggle={setIsDark} duration={500} />
+    </Button>
+  );
+}
