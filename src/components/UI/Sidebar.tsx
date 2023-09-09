7f42b9358b5636f37166211c9ee5e3cdb147caa7
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-09 23:00:05 +0530

    Sidebar Commited

--- src/components/UI/Sidebar.tsx
+++ src/components/UI/Sidebar.tsx
@@ -1,5 +1,9 @@
-type Props = {};
+type SidebarProps = {};
 
-export default function Sidebar({}: Props) {
-  return <aside>Sidebar</aside>
+export default function Sidebar({}: SidebarProps) {
+  return (
+    <aside className="sidebar w-full max-w-xs bg-surfaceContainerLow/50 p-8 text-onSecondaryContainer">
+      Sidebar
+    </aside>
+  );
 }
