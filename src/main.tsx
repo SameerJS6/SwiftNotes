11ca9838559e78c00a0a919c96a9ffdd575444e5
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-12 16:54:18 +0530

    Theme Context Added

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </AuthProvider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
