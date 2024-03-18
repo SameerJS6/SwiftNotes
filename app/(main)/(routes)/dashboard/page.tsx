d08f3e80bc6e95e235d614059f4cd2353f244b6e
SameerJS6 <singh6.sameer@gmail.com>
2024-03-18 23:45:54 +0530

    style: Welcome UI Added

--- app/(main)/(routes)/dashboard/page.tsx
+++ app/(main)/(routes)/dashboard/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/Button";
+import { useUser } from "@clerk/clerk-react";
+import Image from "next/image";
 import React from "react";
 
 export default function Dashboard() {
-  return <div>This is Dashboard Page</div>;
+  const { user } = useUser();
+  return (
+    <main className="grid h-dvh place-content-center">
+      <Image src="/empty.png" alt="Empty" width={450} height={450} />
+      <div className="space-y-3 text-center">
+        <h1 className="text-headline-sm">
+          Welcome to {user?.firstName}&apos;s Note
+        </h1>
+        <Button>
+          <span className="material-symbols-rounded">add</span>Create a note
+        </Button>
+      </div>
+    </main>
+  );
 }
