b7062099874ac78045e148537f004f46fb776e20
SameerJS6 <singh6.sameer@gmail.com>
2024-04-09 23:21:24 +0530

    add: create function added in document page

--- app/(main)/(routes)/dashboard/page.tsx
+++ app/(main)/(routes)/dashboard/page.tsx
@@ -1,12 +1,32 @@
 "use client";
 
-import { Button } from "@/components/ui/Button";
-import { useUser } from "@clerk/clerk-react";
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/Button";
+
+import { useUser } from "@clerk/clerk-react";
+
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import { toast } from "sonner";
 
 export default function Dashboard() {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const handleCreateNote = () => {
+    const response = create({
+      title: "Untitled",
+    });
+
+    toast.promise(response, {
+      loading: "Creating note....",
+      success: "New note created!",
+      error: "Failed to create a new note",
+    });
+  };
   return (
     <main className="grid h-dvh place-content-center">
       <Image src="/empty.png" alt="Empty" width={450} height={450} />
@@ -14,7 +34,7 @@ export default function Dashboard() {
         <h1 className="text-headline-sm">
           Welcome to {user?.firstName}&apos;s Note
         </h1>
-        <Button>
+        <Button onClick={handleCreateNote}>
           <span className="material-symbols-rounded">add</span>Create a note
         </Button>
       </div>
