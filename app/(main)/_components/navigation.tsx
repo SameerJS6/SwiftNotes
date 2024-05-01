45837663e16d1b80d8f89e42704f5c28029a6cb3
SameerJS6 <singh6.sameer@gmail.com>
2024-05-01 23:48:04 +0530

    style:sidebar ui fix

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -10,11 +10,13 @@ import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import Item from "./item";
+import { useTheme } from "next-themes";
 
 export default function Navigation() {
   const pathname = usePathname();
-  const isMobile = useMediaQuery("(max-width: 640px)");
+  const isMobile = useMediaQuery("(max-width: 639px)");
   const isTablet = useMediaQuery("(max-width: 768px)");
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -81,11 +83,17 @@ export default function Navigation() {
       navRef.current.style.transitionDuration = `${timeout}ms`;
       navRef.current.style.setProperty(
         "left",
-        isMobile ? "0" : isTablet ? "384px" : "240px",
+        isMobile ? "0" : isTablet ? "240px" : isDesktop ? "240px" : "0",
       );
       navRef.current.style.setProperty(
         "width",
-        isMobile ? "0" : isTablet ? "calc(100% - 384px)" : "calc(100% - 240px)",
+        isMobile
+          ? "0"
+          : isTablet
+            ? "calc(100% - 240px)"
+            : isDesktop
+              ? "calc(100% - 240px)"
+              : "0",
       );
 
       setTimeout(() => setIsResetting(false), timeout);
@@ -123,17 +131,19 @@ export default function Navigation() {
       error: "Failed to create a new note",
     });
   };
+
+  const { setTheme } = useTheme();
   return (
     <>
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar relative z-[99999] flex min-h-dvh w-60 max-w-sm flex-col overflow-y-auto rounded-e-3xl bg-primaryContainer/15 text-onSurfaceVariant md:max-w-md",
+          "group/sidebar relative z-[99999] flex min-h-dvh w-60 flex-col overflow-y-auto rounded-e-3xl bg-primaryContainer/15 text-onSurfaceVariant transition-colors duration-250 sm:max-w-sm md:max-w-md dark:bg-surfaceContainer",
           isResetting && "transition-all duration-[450ms] ease-out",
           isMobile && isTablet && "w-0",
         )}
       >
-        <Button
+        {/* <Button
           variant="text"
           size="icon"
           iconPosition="iconOnly"
@@ -147,7 +157,7 @@ export default function Navigation() {
           )}
         >
           <span className="material-symbols-rounded size-24p">menu_open</span>
-        </Button>
+        </Button> */}
         <div
           className={`mt-4 w-full rounded-2xl ${isResetting && "transition-opacity duration-[10000] ease-out"}`}
         >
@@ -158,10 +168,33 @@ export default function Navigation() {
               variant="FAB"
               iconPosition="start"
               size="full"
+              className="bg-[#dae2ff] text-[#001847]"
               onClick={handleCreateNote}
             >
               <span className="material-symbols-rounded">add_circle</span>
               New Document
+            </Button>
+
+            <Button
+              variant="text"
+              size="icon"
+              iconPosition="iconOnly"
+              onClick={() => setTheme("light")}
+            >
+              <span className="material-symbols-rounded size-24p">
+                light_mode
+              </span>
+            </Button>
+
+            <Button
+              variant="text"
+              size="icon"
+              iconPosition="iconOnly"
+              onClick={() => setTheme("dark")}
+            >
+              <span className="material-symbols-rounded size-24p">
+                dark_mode
+              </span>
             </Button>
 
             <Button
@@ -208,17 +241,16 @@ export default function Navigation() {
           className="absolute right-2 top-1/2 h-1/6 w-1 origin-center -translate-y-1/2 scale-y-50 cursor-ew-resize rounded-xl bg-onSurfaceVariant/75 opacity-0 transition-all duration-250 ease-in-out group-hover/sidebar:scale-y-100 group-hover/sidebar:opacity-100"
         />
       </aside>
-
       <div
         ref={navRef}
         className={cn(
-          "absolute left-60 top-0 z-[99999] w-[calc(100%-240px)]",
+          "absolute left-60 top-0 z-[99999] w-[calc(100%-240px)] bg-surfaceContainer",
           isResetting && "transition-all duration-[450ms] ease-out",
           isMobile && isTablet && "left-0 w-full",
         )}
       >
-        <nav className="w-full bg-transparent px-3 py-2">
-          {isCollapsed && (
+        <nav className="mx-auto w-full max-w-screen-2xl bg-transparent px-3 py-2">
+          {isCollapsed ? (
             <Button
               variant="text"
               size="icon"
@@ -228,6 +260,19 @@ export default function Navigation() {
               onClick={() => resetSidebarWidth(450)}
             >
               <span className="material-symbols-rounded size-24p">menu</span>
+            </Button>
+          ) : (
+            <Button
+              variant="text"
+              size="icon"
+              iconPosition="iconOnly"
+              className="text-onSurface"
+              centeredRipple
+              onClick={collapseSidebar}
+            >
+              <span className="material-symbols-rounded size-24p">
+                menu_open
+              </span>
             </Button>
           )}
         </nav>
